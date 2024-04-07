76fd37d5642daab9a334d6195609ee660fe95e33
virtualxiaoman <2958694743@qq.com>
2024-04-07 19:24:55 +0800

    1

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    cout << "Indices: " << result[0] << " " << result[1] << endl;
+
+    return 0;
+}
