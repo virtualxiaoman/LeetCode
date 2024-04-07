2f6fee46dfdc6d75a8b9b87eedb6ab1c62c15013
virtualxiaoman <2958694743@qq.com>
2024-04-07 22:25:35 +0800

    1

---
+++ 128.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num_set;
+        for (const int& num : nums) {
+            num_set.insert(num);
+        }
+
+        int longestStreak = 0;
+
+        for (const int& num : num_set) {
+            if (!num_set.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+};
+
+
+int main()
+{
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    Solution solution;
+    cout << solution.longestConsecutive(nums) << endl;
+    return 0;
+}
--- 49.cpp
+++ 49.cpp
@@ -1,114 +1,114 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
-class Solution1
-{
-public:
-    vector<vector<string>> groupAnagrams(vector<string> &strs)
-    {
-        vector<vector<int>> str_count(strs.size(), vector<int>(26, 0));
-        int char_i = 0;
-        for (auto &str: strs) {
-            for (auto s: str) {
-                int ss = s - 'a';
-                str_count[char_i][ss]++;
-            }
-            ++char_i;
-        }
-//        for(const auto &count: str_count)
-//        {
-//            for(auto c: count)
-//            {
-//                cout << c;
+//#include <iostream>
+//#include <vector>
+//#include <unordered_map>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//class Solution1
+//{
+//public:
+//    vector<vector<string>> groupAnagrams(vector<string> &strs)
+//    {
+//        vector<vector<int>> str_count(strs.size(), vector<int>(26, 0));
+//        int char_i = 0;
+//        for (auto &str: strs) {
+//            for (auto s: str) {
+//                int ss = s - 'a';
+//                str_count[char_i][ss]++;
 //            }
-//            cout << endl;
+//            ++char_i;
 //        }
-        // 对c的每一个进行比较，如果相同则加入到一个vector中
-        vector<vector<string>> result;
-        vector<int> used(str_count.size(), 0);
-        for (int i = 0; i < str_count.size(); ++i) {
-            if (used[i]) continue;
-            vector<string> temp;
-            // 要求j不在used里面
-            for (int j = i + 1; j < str_count.size(); ++j) {
-                if (used[j]) continue;
-                if (str_count[i] == str_count[j]) {
-                    temp.push_back(strs[j]);
-                    used[j] = 1;
-                }
-            }
-            temp.push_back(strs[i]);
-            result.push_back(temp);
-        }
-        return result;
-    }
-};
-
+////        for(const auto &count: str_count)
+////        {
+////            for(auto c: count)
+////            {
+////                cout << c;
+////            }
+////            cout << endl;
+////        }
+//        // 对c的每一个进行比较，如果相同则加入到一个vector中
+//        vector<vector<string>> result;
+//        vector<int> used(str_count.size(), 0);
+//        for (int i = 0; i < str_count.size(); ++i) {
+//            if (used[i]) continue;
+//            vector<string> temp;
+//            // 要求j不在used里面
+//            for (int j = i + 1; j < str_count.size(); ++j) {
+//                if (used[j]) continue;
+//                if (str_count[i] == str_count[j]) {
+//                    temp.push_back(strs[j]);
+//                    used[j] = 1;
+//                }
+//            }
+//            temp.push_back(strs[i]);
+//            result.push_back(temp);
+//        }
+//        return result;
+//    }
+//};
+//
+////class Solution {
+////public:
+////    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+////        unordered_map<string, vector<string>> mp;
+////        for (string& str: strs) {
+////            string key = str;
+////            sort(key.begin(), key.end());
+////            mp[key].emplace_back(str);
+////        }
+////        vector<vector<string>> ans;
+////        for (auto it = mp.begin(); it != mp.end(); ++it) {
+////            ans.emplace_back(it->second);
+////        }
+////        return ans;
+////    }
+////};
+//
 //class Solution {
 //public:
 //    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//        unordered_map<string, vector<string>> mp;
-//        for (string& str: strs) {
-//            string key = str;
-//            sort(key.begin(), key.end());
-//            mp[key].emplace_back(str);
-//        }
 //        vector<vector<string>> ans;
-//        for (auto it = mp.begin(); it != mp.end(); ++it) {
-//            ans.emplace_back(it->second);
+//        unordered_map<string, int> mp;
+//
+//        for (auto& str : strs)
+//        {
+//            string s = str;
+//            sort(s.begin(), s.end());
+//
+//            if (mp.count(s))
+//            {
+//                int ind = mp[s];
+//                ans[ind].push_back(str);
+//            }
+//            else
+//            {
+//                mp[s] = ans.size();
+//                ans.push_back({str});
+//            }
 //        }
+//
 //        return ans;
 //    }
 //};
-
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        unordered_map<string, int> mp;
-
-        for (auto& str : strs)
-        {
-            string s = str;
-            sort(s.begin(), s.end());
-
-            if (mp.count(s))
-            {
-                int ind = mp[s];
-                ans[ind].push_back(str);
-            }
-            else
-            {
-                mp[s] = ans.size();
-                ans.push_back({str});
-            }
-        }
-
-        return ans;
-    }
-};
-
-int main()
-{
-    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    Solution solution;
-    vector<vector<string>> result = solution.groupAnagrams(strs);
-    for(const auto &vec: result)
-    {
-        for(const auto &str: vec)
-        {
-            cout << str << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-
-}
-
-
-
-
+//
+//int main()
+//{
+//    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+//    Solution solution;
+//    vector<vector<string>> result = solution.groupAnagrams(strs);
+//    for(const auto &vec: result)
+//    {
+//        for(const auto &str: vec)
+//        {
+//            cout << str << " ";
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+//
+//}
+//
+//
+//
+//
